33a7bc7d252276079d2eb7e29dd163d55b35ebbb
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-09 16:23:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,8 @@ def get_english_meaning(file, emoticon)
   load_library(file).each do |key, value|
     if value[:japanese] == emoticon
       return key
+    else
+      return "Sorry, that emoticon was not found"
   end
 end
 
