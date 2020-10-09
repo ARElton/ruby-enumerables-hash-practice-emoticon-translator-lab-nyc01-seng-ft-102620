03a4b26a29afa51d3ecfed76fee90203187708f7
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-09 15:31:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library
  YAML.load_file('lib/emoticons.yml').each do |key, value|
    english,japanese = value
    emoticons = key
-   hash.new = value 
+   Hash.new = value 
   
    binding.pry
   
