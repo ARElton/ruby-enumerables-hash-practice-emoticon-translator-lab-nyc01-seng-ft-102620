eb5bc8be68eb9660b061a155f6eba7582b36ae3a
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-09 14:40:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,12 @@ require 'pry'
 
 def load_library
  emoticons = {
-   meaning: {}
+   meaning: {},
    emoticon: {}
  }
- 
+ YMAL.load_file.each do |meaning, describe|
+   english, japanese = describe
+   
 end
 
 def get_japanese_emoticon
